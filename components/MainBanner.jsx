3f1056c92c6e56f7af20a9a7ec35ddf3e3febe82
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-10-21 17:21:47 +0100

    setup for mainbanner done

--- components/MainBanner.jsx
+++ components/MainBanner.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Link from "next/link";
 
 const MainBanner = () => {
   return (
     <div className="main-banner-container">
       <div>
-        <p className="panda-pillow"></p>
+        <p className="panda-pillow">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="pillows" className="main-banner-image" />
+
+        <div>
+          <Link href="/merchandise/ID">
+            <button type="button">BUTTON TEXT</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
       </div>
     </div>
   );
