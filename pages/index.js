b9804efbcb76e07b4b9d766d2b467e7cbdc8638f
AliCo-Hash <ali.cocelli@hotmail.co.uk>
2022-10-21 16:48:40 +0100

    merchandise container created and css done

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const Home = () => {
         <h2>Best Selling Items</h2>
         <p>Lovely Pillows</p>
       </div>
-      <div>
+      <div className="merchandise-container">
         {["Merchandise 1", "Merchandise 2"].map(merchandise => merchandise)}
       </div>
       Footer
